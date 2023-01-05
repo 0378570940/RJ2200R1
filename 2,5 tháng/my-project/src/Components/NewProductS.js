4ce8bcd529efdd49263d60ad5dc4a12e9f5a5a1d
0378570940 <0378570940>
2023-01-05 10:02:29 +0700

    update form

--- 2,5 tháng/my-project/src/Components/NewProductS.js
+++ 2,5 tháng/my-project/src/Components/NewProductS.js
@@ -1,38 +1,18 @@
 import React, { useState } from 'react'
 
-const NewProductS = () => {
-    const [state, setStatus] = useState("")
+const NewProductS = (props) => {
+    const { label, errorMessage, onChange, id, ...inputProps } = props;
+    const [focused, setFocused] = useState(false)
+    const handleFocus = (e) => {
+        setFocused(true);
+    }
     return (
         <div class="col-md-5">
-            <form>
-                <h3>Thông tin sản phẩm</h3>
-                <div class="row mb-3">
-                    <label for="inputEmail3" class="col-sm-2 col-form-label">
-                        Tên sản phẩm
-                    </label>
-                    <div class="col-sm-10">
-                        <input
-                            type="text"
-                            class="form-control"
-                            id="inputEmail3"
-                        />
-                    </div>
-                </div>
-                <div class="row mb-3">
-                    <label for="inputPassword3" class="col-sm-2 col-form-label">
-                        Mã sản phẩm
-                    </label>
-                    <div class="col-sm-10">
-                        <input type="text" class="form-control" id="inputPassword3" />
-                    </div>
-                </div>
-                <button
-                    type="submit"
-                    class="btn btn-primary"
-                >
-                    Sign in
-                </button>
-            </form >
+            <div>
+                <label>{label}</label>
+                <input {...inputProps} onChange={onChange} onBlur={handleFocus} focused={focused.toString()} />
+                <span>{errorMessage}</span>
+            </div>
         </div >
     )
 }
